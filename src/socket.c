cd1e41b33d9aaa3503edc8f6248f416a838bc928
maze <louisdalmasso2003@gmail.com>
2026-05-13 18:18:14 +0200

    Use src sockaddr for reverse DNS lookup
    Comment out unused iphdr/ip_dest variables and pass src/src_len to
    getnameinfo to fix sockaddr usage

--- src/socket.c
+++ src/socket.c
@@ -84,10 +84,10 @@ float deserialize_icmp_packet(t_tr *tr, struct timeval start)
 	LOG(DEBUG "Elapsed time: %.2f us" RESET, new_reply_node->elapsed_time_in_usec);
 
 	// Add the reversed DNS string to the new node if we can
-	struct iphdr	*ip_to_resolve = (struct iphdr *)buffer;
+	// struct iphdr	*ip_to_resolve = (struct iphdr *)buffer;
 	// uint32_t		ip_src = ip_to_resolve->saddr;
-	uint32_t		ip_dest = ip_to_resolve->daddr;
-	if (getnameinfo((struct sockaddr *)&ip_dest, sizeof(ip_dest), new_reply_node->reversed_dns_str, NI_MAXHOST, NULL, 0, NI_NAMEREQD) != 0)
+	// uint32_t		ip_dest = ip_to_resolve->daddr;
+	if (getnameinfo((struct sockaddr *)&src, src_len, new_reply_node->reversed_dns_str, NI_MAXHOST, NULL, 0, NI_NAMEREQD) != 0)
 		new_reply_node->reversed_dns_str[0] = '\0';
 
 	// Apply the new node to the end of the linked list
